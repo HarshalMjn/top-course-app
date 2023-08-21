ac260519d1c6a6d183c6c7febe6f08eb6bfe992b
Harshal Mahajan <mahajanh006@gmail.com>
2023-08-22 01:19:40 +0530

    App.js

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import React from "react";
+import Navbar from  "./components/Navbar";
+import Cards from "./components/Cards"
+import Filter from "./components/Filter"
+import { apiUrl, filterData  } from "./data";
+import { useState,useEffect } from "react";
+import Spinner from "./components/Spinner";
+import {toast} from "react-toastify";
+
+
+const App = () => {
+   
+  const [courses, setCourses] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  async function fetchData() {
+    setLoading(true);
+    try{
+      let response = await fetch(apiUrl);
+      let output = await response.json();
+      ///output -> 
+      setCourses(output.data);
+      console.log(output)
+    }
+    catch(error) {
+        toast.error("Network me koi dikkat hai");
+    }
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, [])
+
+
+
+
+  return (
+    <div>
+   <div>
+     <Navbar/>
+   </div>
+   <div>
+     <Filter filterData={filterData}/>
+   </div>
+   <div>
+     {
+      loading ? (<Spinner/>) : (<Cards courses={courses}/>)
+     }
+   </div>
+
+  </div>
+  )
+  
+  
+};
+
+export default App;
